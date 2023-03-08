9cd3d2e1b3df7e3bf63e023726ee722698290bbd
ВасянПупкин <tgoroshek@gmail.com>
2023-03-08 23:27:25 +0300

    HW Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Petrov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+        System.out.println(" ");
+
+        //2
+        String upperCaseName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + upperCaseName);
+        System.out.println(" ");
+
+        //3
+        String semyonSemyonich = "Иванов Семен Семёнович";
+        String correctLetters = semyonSemyonich.replace("ё", "е");
+        correctLetters = correctLetters.replace("Ё", "Е");
+        System.out.println("Данные ФИО сотрудника — " + correctLetters);
     }
 }
